1cda7d57f9115e2dcac2f428bdb9f9ab8f7b8e76
loquacity <github@lanabrindley.com>
2021-05-27 13:01:04 +1000

    Move to top level

---
+++ promscale/page-index/page-index.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    title: "Promscale",
+    filePath: 'index.md',
+    href: "promscale",
+    name: 'Promscale',
+    excerpt: 'Promscale is an open source long-term store for Prometheus data designed for analytics, built on top of TimescaleDB.',
+    children: [
+      {
+        title: "Promscale",
+        href: "promscale",
+      }
+        ]
+      }
+    ]
+  }
+]
