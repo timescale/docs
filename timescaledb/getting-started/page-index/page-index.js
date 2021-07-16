8c661a43c5cc39a2de07cb4ed706d337973d2edf
Jacob <prall.jacob@gmail.com>
2021-07-16 08:24:09 -0500

    add test of keywords

--- timescaledb/getting-started/page-index/page-index.js
+++ timescaledb/getting-started/page-index/page-index.js
@@ -1,45 +1,49 @@
 module.exports = [
-    {
-        title: "Getting started",
-        href: "getting-started",
-        pageComponents: ['featured-cards'],
-        children: [
-          {
-            title: "1. Launch your first instance",
-            href: "launch-timescaledb"
-          },
-          {
-            title: "2. Access your database",
-            href: "access-timescaledb",
-          },
-          {
-            title: "3. Create a hypertable",
-            href: "create-hypertable"
-          },
-          {
-            title: "4. Add time-series data",
-            href: "add-data"
-          },
-          {
-            title: "5. Query your data",
-            href: "query-data"
-          },
-          {
-            title: "6. Create a continuous aggregate",
-            href: "create-cagg"
-          },
-          {
-            title: "7. Save space with compression",
-            href: "compress-data"
-          },
-          {
-            title: "8. Learn about data retention",
-            href: "data-retention"
-          },
-          {
-            title: "9. Next steps",
-            href: "next-steps"
-          }
-        ]
-      }
-]
+  {
+    title: 'Getting started',
+    href: 'getting-started',
+    pageComponents: ['featured-cards'],
+    keywords: ['start', 'install'],
+    children: [
+      {
+        title: '1. Launch your first instance',
+        href: 'launch-timescaledb',
+        keywords: ['start', 'install'],
+      },
+      {
+        title: '2. Access your database',
+        href: 'access-timescaledb',
+        keywords: ['start', 'none'],
+      },
+      {
+        title: '3. Create a hypertable',
+        href: 'create-hypertable',
+        keywords: ['none', 'install'],
+      },
+      {
+        title: '4. Add time-series data',
+        href: 'add-data',
+      },
+      {
+        title: '5. Query your data',
+        href: 'query-data',
+      },
+      {
+        title: '6. Create a continuous aggregate',
+        href: 'create-cagg',
+      },
+      {
+        title: '7. Save space with compression',
+        href: 'compress-data',
+      },
+      {
+        title: '8. Learn about data retention',
+        href: 'data-retention',
+      },
+      {
+        title: '9. Next steps',
+        href: 'next-steps',
+      },
+    ],
+  },
+];
