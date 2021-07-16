352f4c6479571221c02cebca6c6532e684841fca
Jacob <prall.jacob@gmail.com>
2021-07-16 08:25:39 -0500

    remove dangling commas

--- timescaledb/getting-started/page-index/page-index.js
+++ timescaledb/getting-started/page-index/page-index.js
@@ -8,42 +8,42 @@ module.exports = [
       {
         title: '1. Launch your first instance',
         href: 'launch-timescaledb',
-        keywords: ['start', 'install'],
+        keywords: ['start', 'install']
       },
       {
         title: '2. Access your database',
         href: 'access-timescaledb',
-        keywords: ['start', 'none'],
+        keywords: ['start', 'none']
       },
       {
         title: '3. Create a hypertable',
         href: 'create-hypertable',
-        keywords: ['none', 'install'],
+        keywords: ['none', 'install']
       },
       {
         title: '4. Add time-series data',
-        href: 'add-data',
+        href: 'add-data'
       },
       {
         title: '5. Query your data',
-        href: 'query-data',
+        href: 'query-data'
       },
       {
         title: '6. Create a continuous aggregate',
-        href: 'create-cagg',
+        href: 'create-cagg'
       },
       {
         title: '7. Save space with compression',
-        href: 'compress-data',
+        href: 'compress-data'
       },
       {
         title: '8. Learn about data retention',
-        href: 'data-retention',
+        href: 'data-retention'
       },
       {
         title: '9. Next steps',
-        href: 'next-steps',
-      },
-    ],
-  },
+        href: 'next-steps'
+      }
+    ]
+  }
 ];
