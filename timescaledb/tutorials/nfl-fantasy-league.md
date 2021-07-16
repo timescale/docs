46393f749d62edeccb9f15a296f15b31334df9cc
Attila Toth <hello@attilatoth.dev>
2021-07-16 12:43:10 +0200

    NFL tutorial draft

---
+++ timescaledb/tutorials/nfl-fantasy-league.md
@@ -0,0 +1,188 @@
+# Win your NFL fantasy league with TimescaleDB
+
+This tutorial is a step-by-step guide on how to ingest and analyze american football data with TimescaleDB.
+
+The dataset that we're using is provided be the National Football League (NFL) and contains data about 
+all the passing plays of the 2018 NFL season. We're ingesting this dataset and exploring it in fun ways. You will also see how this dataset and using TimescaleDB can improve your NFL fantasy league strategy. If you aren't an NFL fan, don't worry, you will still find this tutorial helpful as it's showcasing how to get started with TimescaleDB and explore a real world dataset with SQL and Python.
+
+1. [Create tables][]
+2. [Ingest data from CSV files][] 
+3. [Analyze NFL data][]
+   
+## Prerequisites
+
+* Python 3
+* TimescaleDB (see [installation options][install-timescale]) 
+* [Psql][psql-install] or any other PostgreSQL client (e.g. DBeaver)
+* Pandas and Matplotlib (`pip install pandas matplotlib`)
+
+## About the dataset
+
+The dataset we're using in this tutorial is provided by the National Football League (NFL) as part of their yearly
+Big Data Bowl. Big Data Bowl is a contest for data professionals and students to explore american football data and build interesting machine learning models.
+
+As this year's contest hasn't been published yet by the NFL, we're using last year's dataset that includes data about all the passing plays of the 2018 season.
+
+Download dataset here!
+
+## Create tables
+
+You will need to create four tables:
+
+* game
+  
+  Information about each game.
+* player
+  
+  Player information.
+* play
+  
+  Play information
+* tracking
+  
+  Tracking information from each play.
+
+```sql
+CREATE TABLE game (
+    gameId INT PRIMARY KEY,
+    gameDate DATE,
+    gameTimeEastern TIME,
+    homeTeamAbbr VARCHAR(3),
+    visitorTeamAbbr VARCHAR(3),
+    week INT
+);
+
+CREATE TABLE player (
+    nflId INT,
+    height TEXT,
+    weight INT,
+    birthDate DATE,
+    collegeName TEXT,
+    position TEXT,
+    displayName TEXT
+);
+
+CREATE TABLE play (
+    gameId INT,
+    playId INT,
+    playDescription TEXT,
+    quarter INT,
+    down INT,
+    yardsToGo INT,
+    possessionTeam TEXT,
+    playType TEXT,
+    yardlineSide TEXT,
+    yardlineNumber INT,
+    offenseFormation TEXT,
+    personnelO TEXT,
+    defendersInTheBox INT,
+    numberOfPassRushers INT,
+    personnelD TEXT,
+    typeDropback TEXT,
+    preSnapVisitorScore INT,
+    preSnapHomeScore INT,
+    gameClock TIME,
+    absoluteYardlineNumber INT,
+    penaltyCodes TEXT,
+    penaltyJerseyNumber TEXT,
+    passResult TEXT,
+    offensePlayResult INT,
+    playResult INT,
+    epa DOUBLE PRECISION,
+    isDefensivePI BOOLEAN
+);
+
+CREATE TABLE tracking (
+    time TIMESTAMP,
+    x DOUBLE PRECISION,
+    y DOUBLE PRECISION,
+    s DOUBLE PRECISION,
+    a DOUBLE PRECISION,
+    dis DOUBLE PRECISION,
+    o DOUBLE PRECISION,
+    dir DOUBLE PRECISION,
+    event TEXT,
+    nflId INT,
+    displayName TEXT,
+    jerseyNumber INT,
+    position TEXT,
+    frameId INT,
+    team TEXT,
+    gameId INT,
+    playId INT,
+    playDirection VARCHAR(5),
+    route TEXT
+);
+```
+
+## Ingest data from CSV files
+
+We have 3 separate CSV files for game, player, and play tables. For the tracking table, you will need to
+import data from 17 CSV files (1 file for each week in the season).
+
+Let's use a Python script that utilizes psycopg2's `copy_expert` function to ingest the data:
+
+```python
+import config
+import psycopg2
+
+conn = psycopg2.connect(database=config.DB_NAME, 
+                        host=config.HOST, 
+                        user=config.USER, 
+                        password=config.PASS, 
+                        port=config.PORT)
+
+def insert(csv_file, table_name):
+    cur = conn.cursor()
+    copy_sql = """
+            COPY {table} FROM stdin WITH CSV HEADER
+            DELIMITER as ','
+            """.format(table=table_name)
+    with open(csv_file, 'r') as f:
+        cur.copy_expert(sql=copy_sql, file=f)
+        conn.commit()
+        cur.close()
+
+print("Inserting games.csv")
+insert("data/games.csv", "game")
+
+print("Inserting plays.csv")
+insert("data/plays.csv", "play")
+
+print("Inserting players.csv")
+insert("data/players.csv", "player")
+
+for i in range(1, 18): 
+    print("Inserting week{i}".format(str(i)))
+    insert("data/week{i}.csv".format(i=i), "tracking")
+
+conn.close()
+```
+
+## Analyze NFL data
+
+Now that you have all the data ingested, let's see some ideas how you can analyze the data to help you perfect
+your fantasy drafting strategy and win your fantasy season.
+
+To analyze the dataset you will need to install two libraries, if you haven't already: Pandas and Matplotlib.
+
+### Install pandas and matplotlib
+```bash
+pip install pandas matplotlib
+```
+
+1. Number of yards run in game for passing plays, by player and game 
+1. Avg yards run for a player over a game
+1. Average and median yards run per game by type of player (not taking avg of individual)
+1. Num of snap plays by player where they were on the offense
+1. Number of plays vs points scored
+1. Average yards per game for top 3 players of each position
+
+(todo include queries and viz)
+
+
+## Resources
+
+* Github project repository
+* NFL Big Data Bowl on Kaggle
+* 
--- timescaledb/tutorials/page-index/page-index.js
+++ timescaledb/tutorials/page-index/page-index.js
@@ -1,116 +1,120 @@
 module.exports = [
-    {
-        title: "Tutorials",
-        href: "tutorials",
-        children: [
-          {
-            title: "Introduction to IoT",
-            href: "nyc-taxi-cab"
-          },
-          {
-            title: "Introduction to time-series forecasting",
-            href: "time-series-forecast"
-          },
-          {
-            title: "Analyze cryptocurrency data",
-            href: "analyze-cryptocurrency-data"
-          },
-          {
-            title: "Analyze intraday stock data",
-            href: "analyze-intraday-stocks",
-            children: [
-              {
-                title: "Design database schema",
-                href: "design-schema"
-              },
-              {
-                title: "Fetch and ingest stock data",
-                href: "fetch-and-ingest"
-              },
-              {
-                title: "Explore stock market data",
-                href: "explore-stocks-data"
-              }
-            ]
-          },
-          {
-            title: "Getting started with Promscale",
-            href: "promscale",
-            children: [
-              {
-                title: "The Benefits of using Promscale",
-                href: "promscale-benefits"
-              },
-              {
-                title: "How Promscale works",
-                href: "promscale-how-it-works"
-              },
-              {
-                title: "Installing Promscale",
-                href: "promscale-install"
-              },
-              {
-                title: "Run queries with PromQL and SQL",
-                href: "promscale-run-queries"
-              }
-            ]
-          },
-          {
-            title: "Monitor Timescale Cloud with Prometheus",
-            href: "setting-up-timescale-cloud-endpoint-for-prometheus"
-          },
-          {
-            title: "Monitor a Django application with Prometheus",
-            href: "monitor-django-with-prometheus"
-          },
-          {
-            title: "Collect metrics with Telegraf",
-            href: "telegraf-output-plugin"
-          },
-          {
-            title: "Grafana",
-            href: "grafana",
-            children: [
-              {
-                href: "installation"
-              },
-              {
-                title: "Create a dashboard and panel",
-                href: "create-dashboard-and-panel"
-              },
-              {
-                title: "Build Geospatial dashboards",
-                href: "geospatial-dashboards"
-              },
-              {
-                title: "Use Grafana variables",
-                href: "grafana-variables"
-              },
-              {
-                title: "Visualize missing data",
-                href: "visualize-missing-data"
-              },
-              {
-                title: "Setup alerts",
-                href: "setup-alerts"
-              }
-            ]
-          },
-          {
-            title: "Visualize data in Tableau",
-            href: "visualize-with-tableu"
-          },
-          {
-            title: "Custom TimescaleDB dashboards",
-            href: "custom-timescaledb-dashboards"
-          },
-          {
-            title: "Simulate IoT Sensor Data",
-            href: "simulate-iot-sensor-data"
-          },
-          {
-            href: "sample-datasets"
-          }
-        ]
-      }
+  {
+      title: "Tutorials",
+      href: "tutorials",
+      children: [
+        {
+          title: "Getting started with Promscale",
+          href: "promscale",
+          children: [
+            {
+              title: "The Benefits of using Promscale",
+              href: "promscale-benefits"
+            },
+            {
+              title: "How Promscale works",
+              href: "promscale-how-it-works"
+            },
+            {
+              title: "Installing Promscale",
+              href: "promscale-install"
+            },
+            {
+              title: "Run queries with PromQL and SQL",
+              href: "promscale-run-queries"
+            }
+          ]
+        },
+        {
+          title: "Introduction to IoT",
+          href: "nyc-taxi-cab"
+        },
+        {
+          title: "Introduction to time-series forecasting",
+          href: "time-series-forecast"
+        },
+        {
+          title: "Analyzing cryptocurrency data",
+          href: "analyze-cryptocurrency-data"
+        },
+        {
+          title: "Analyze intraday stock data",
+          href: "analyze-intraday-stocks",
+          children: [
+            {
+              title: "Design database schema",
+              href: "design-schema"
+            },
+            {
+              title: "Fetch and ingest stock data",
+              href: "fetch-and-ingest"
+            },
+            {
+              title: "Explore stock market data",
+              href: "explore-stocks-data"
+            }
+          ]
+        },
+        {
+          title: "Grafana",
+          href: "grafana",
+          children: [
+            {
+              href: "installation"
+            },
+            {
+              title: "Create a dashboard and panel",
+              href: "create-dashboard-and-panel"
+            },
+            {
+              title: "Build Geospatial dashboards",
+              href: "geospatial-dashboards"
+            },
+            {
+              title: "Use Grafana variables",
+              href: "grafana-variables"
+            },
+            {
+              title: "Visualize missing data",
+              href: "visualize-missing-data"
+            },
+            {
+              title: "Setup alerts",
+              href: "setup-alerts"
+            }
+          ]
+        },
+        {
+          title: "Visualize data in Tableau",
+          href: "visualize-with-tableu"
+        },
+        {
+          title: "Custom TimescaleDB dashboards",
+          href: "custom-timescaledb-dashboards"
+        },
+        {
+          title: "Simulate IoT Sensor Data",
+          href: "simulate-iot-sensor-data"
+        },
+        {
+          title: "Monitor Timescale Cloud with Prometheus",
+          href: "setting-up-timescale-cloud-endpoint-for-prometheus"
+        },
+        {
+          title: "Collecting metrics with Telegraf",
+          href: "telegraf-output-plugin"
+        },
+        {
+          title: "Monitor a Django application with Prometheus",
+          href: "monitor-django-with-prometheus"
+        },
+        {
+          title: "Win your NFL fantasy league with TimescaleDB",
+          href: "nfl-fantasy-league"
+        },
+        {
+          href: "sample-datasets"
+        }
+      ]
+    }
 ]
