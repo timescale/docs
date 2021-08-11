3b303c39dfb66e9dc2cd913abff02006710f7ec6
loquacity <github@lanabrindley.com>
2021-08-11 15:11:09 +1000

    All the rest \o/

--- timescaledb/how-to-guides/page-index/page-index.js
+++ timescaledb/how-to-guides/page-index/page-index.js
@@ -393,35 +393,59 @@ module.exports = [
         children: [
           {
             title: "About continuous aggregates",
-            href: "about-continuous-aggregates"
+            href: "about-continuous-aggregates",
+            tags: ['caggs', 'timescaledb'],
+            keywords: ['caggs', 'TimescaleDB'],
+            excerpt: ['About continuous aggregates']
           },
           {
             title: "Create a continuous aggregate",
-            href: "create-a-continuous-aggregate"
+            href: "create-a-continuous-aggregate",
+            tags: ['caggs', 'create', 'timescaledb'],
+            keywords: ['caggs', 'TimescaleDB'],
+            excerpt: ['Create continuous aggregates']
           },
           {
             title: "Refresh policies for continuous aggregates",
-            href: "refresh-policies"
+            href: "refresh-policies",
+            tags: ['caggs', 'manage', 'timescaledb'],
+            keywords: ['caggs', 'TimescaleDB'],
+            excerpt: ['Manage refresh policies for continuous aggregates']
           },
           {
             title:"Time in continuous aggregates",
-            href: "time"
+            href: "time",
+            tags: ['caggs', 'manage', 'timescaledb'],
+            keywords: ['caggs', 'TimescaleDB'],
+            excerpt: ['Manage time in continuous aggregates']
           },
           {
             title: "Drop data from continuous aggregates",
-            href: "drop-data"
+            href: "drop-data",
+            tags: ['caggs', 'delete', 'data', 'timescaledb'],
+            keywords: ['caggs', 'TimescaleDB'],
+            excerpt: ['Drop data from continuous aggregates']
           },
           {
             title: "Manage materialized hypertables",
-            href: "materialized-hypertables"
+            href: "materialized-hypertables",
+            tags: ['caggs', 'hypertables', 'manage', 'timescaledb'],
+            keywords: ['caggs', 'TimescaleDB'],
+            excerpt: ['Manage materialized hypertables in continuous aggregates']
           },
           {
             title: "Real time aggregates",
-            href: "real-time-aggregates"
+            href: "real-time-aggregates",
+            tags: ['caggs', 'manage', 'timescaledb'],
+            keywords: ['caggs', 'TimescaleDB'],
+            excerpt: ['Manage real time aggregates in continuous aggregates']
           },
           {
             title: "Troubleshoot continuous aggregates",
-            href: "troubleshooting"
+            href: "troubleshooting",
+            tags: ['caggs', 'tshoot', 'timescaledb'],
+            keywords: ['caggs', 'TimescaleDB'],
+            excerpt: ['Troubleshoot continuous aggregates']
           }
         ]
       },
@@ -430,23 +454,38 @@ module.exports = [
         children: [
           {
             title: "About compression",
-            href: "about-compression"
+            href: "about-compression",
+            tags: ['compression', 'chunks', 'data', 'timescaledb'],
+            keywords: ['compression', 'chunks', 'TimescaleDB'],
+            excerpt: ['About data compression']
           },
           {
             title: "Manually compress chunks",
-            href: "manually-compress-chunks"
+            href: "manually-compress-chunks",
+            tags: ['compression', 'chunks', 'data', 'timescaledb'],
+            keywords: ['compression', 'chunks', 'TimescaleDB'],
+            excerpt: ['Manually compress data chunks']
           },
           {
             title: "Decompress chunks",
-            href: "decompress-chunks"
+            href: "decompress-chunks",
+            tags: ['compression', 'chunks', 'data', 'timescaledb'],
+            keywords: ['compression', 'chunks', 'TimescaleDB'],
+            excerpt: ['Manually decompress data chunks']
           },
           {
             title: "Backfill historical data",
-            href: "backfill-historical-data"
+            href: "backfill-historical-data",
+            tags: ['compression', 'chunks', 'data', 'timescaledb'],
+            keywords: ['compression', 'chunks', 'TimescaleDB'],
+            excerpt: ['Backfill historical data to compressed chunks']
           },
           {
             title: "Modify a schema",
-            href: "modify-a-schema"
+            href: "modify-a-schema",
+            tags: ['compression', 'chunks', 'data', 'timescaledb'],
+            keywords: ['compression', 'chunks', 'TimescaleDB'],
+            excerpt: ['Change the data schema in compressed chunks']
           }
         ]
       },
@@ -456,27 +495,45 @@ module.exports = [
         children: [
           {
             title: "Create and register",
-            href: "create-and-register"
+            href: "create-and-register",
+            tags: ['action', 'create', 'timescaledb'],
+            keywords: ['action', 'TimescaleDB'],
+            excerpt: ['Create a user-defined action']
           },
           {
             title: "Test and debug",
-            href: "test-and-debug"
+            href: "test-and-debug",
+            tags: ['action', 'test', 'timescaledb'],
+            keywords: ['action', 'TimescaleDB'],
+            excerpt: ['Test and debug user-defined actions']
           },
           {
             title: "Altering and deleting",
-            href: "alter-and-delete"
+            href: "alter-and-delete",
+            tags: ['action', 'edit', 'delete', 'timescaledb'],
+            keywords: ['action', 'TimescaleDB'],
+            excerpt: ['Edit and delete user-defined actions']
           },
           {
             title: "Example of generic retention",
-            href: "example-generic-retention"
+            href: "example-generic-retention",
+            tags: ['action', 'example', 'retention', 'timescaledb'],
+            keywords: ['action', 'example', 'TimescaleDB'],
+            excerpt: ['Example user-defined action for a retention policy']
           },
           {
             title: "Example of tiered storage",
-            href: "example-tiered-storage"
+            href: "example-tiered-storage",
+            tags: ['action', 'example', 'timescaledb'],
+            keywords: ['action', 'example', 'TimescaleDB'],
+            excerpt: ['Example user-defined action for tiered storage']
           },
           {
             title: "Example of downsample and compress",
-            href: "example-downsample-and-compress"
+            href: "example-downsample-and-compress",
+            tags: ['action', 'example', 'compress', 'timescaledb'],
+            keywords: ['action', 'example', 'TimescaleDB'],
+            excerpt: ['Example user-defined action for downsample and compress']
           }
         ]
       },
@@ -485,15 +542,24 @@ module.exports = [
         children: [
           {
             title: "Create a retention policy",
-            href: "create-a-retention-policy"
+            href: "create-a-retention-policy",
+            tags: ['retention', 'data', 'create', 'timescaledb'],
+            keywords: ['retention', 'data', 'TimescaleDB'],
+            excerpt: ['Create a data retention policy']
           },
           {
             title: "Manually drop chunks",
-            href: "manually-drop-chunks"
+            href: "manually-drop-chunks",
+            tags: ['retention', 'data', 'delete', 'chunks', 'timescaledb'],
+            keywords: ['retention', 'data', 'TimescaleDB'],
+            excerpt: ['Manually drop chunks']
           },
           {
             title: "Data retention with continuous aggregates",
-            href: "data-retention-with-continuous-aggregates"
+            href: "data-retention-with-continuous-aggregates",
+            tags: ['retention', 'caggs', 'data', 'timescaledb'],
+            keywords: ['retention', 'data', 'caggs', 'TimescaleDB'],
+            excerpt: ['Using data retention policies with continuous aggregates']
           }
         ]
       },
@@ -503,7 +569,10 @@ module.exports = [
         children: [
           {
             title: "Move data",
-            href: "move-data"
+            href: "move-data",
+            tags: ['tiering', 'data', 'migrate', 'chunks', 'timescaledb'],
+            keywords: ['tiering', 'data', 'TimescaleDB'],
+            excerpt: ['Move data with data tiering']
           }
         ]
       },
@@ -512,7 +581,11 @@ module.exports = [
         href: "replication-and-ha",
         children: [
           {
-            href: "replication"
+            title: "Replication and HA",
+            href: "replication",
+            tags: ['ha', 'replicate', 'timescaledb'],
+            keywords: ['ha', 'replicate', 'TimescaleDB'],
+            excerpt: ['Replication and high availability']
           }
         ]
       },
@@ -522,19 +595,31 @@ module.exports = [
         children: [
           {
             title: "The timescaledb-backup tool",
-            href: "timescaledb-backup"
+            href: "timescaledb-backup",
+            tags: ['backup', 'restore', 'timescaledb'],
+            keywords: ['backup', 'TimescaleDB'],
+            excerpt: ['Backing up and restoring with the timescaledb-backup tool']
           },
           {
             title: "Using pg_dump/pg_restore",
-            href: "pg-dump-and-restore"
+            href: "pg-dump-and-restore",
+            tags: ['backup', 'restore', 'timescaledb'],
+            keywords: ['backup', 'TimescaleDB'],
+            excerpt: ['Backing up and restoring with the pg_dump and pg_restore']
           },
           {
             title: "Docker & WAL-E",
-            href: "docker-and-wale"
+            href: "docker-and-wale",
+            tags: ['backup', 'restore', 'timescaledb'],
+            keywords: ['backup', 'TimescaleDB'],
+            excerpt: ['Backing up and restoring with Docker and WAL-E']
           },
           {
             title: "Physical backups",
-            href: "physical"
+            href: "physical",
+            tags: ['backup', 'restore', 'timescaledb'],
+            keywords: ['backup', 'TimescaleDB'],
+            excerpt: ['Backing up and restoring with physical backups']
           }
         ]
       },
@@ -544,23 +629,45 @@ module.exports = [
         children: [
           {
             title: "ALTER hypertables",
-            href: "alter"
+            href: "alter",
+            tags: ['schema', 'hypertables', 'edit', 'timescaledb'],
+            keywords: ['schema', 'hypertables', 'TimescaleDB'],
+            excerpt: ['Change the schema of a hypertable']
           },
           {
-            href: "indexing"
+            title: "Indexing",
+            href: "indexing",
+            tags: ['schema', 'hypertables', 'index', 'timescaledb'],
+            keywords: ['schema', 'hypertables', 'TimescaleDB'],
+            excerpt: ['Create an index on a hypertable']
           },
           {
-            href: "triggers"
+            title: "Triggers",
+            href: "triggers",
+            tags: ['schema', 'hypertables', 'manage', 'timescaledb'],
+            keywords: ['schema', 'hypertables', 'TimescaleDB'],
+            excerpt: ['Create an index on a hypertable']
           },
           {
-            href: "constraints"
+            title: "Constraints",
+            href: "constraints",
+            tags: ['schema', 'hypertables', 'manage', 'timescaledb'],
+            keywords: ['schema', 'hypertables', 'TimescaleDB'],
+            excerpt: ['Create constraints on a hypertable']
           },
           {
             title: "JSON",
-            href: "json"
+            href: "json",
+            tags: ['schema', 'hypertables', 'json', 'data', 'timescaledb'],
+            keywords: ['schema', 'hypertables', 'json', 'TimescaleDB'],
+            excerpt: ['Using JSON data types in a hypertable']
           },
           {
-            href: "tablespaces"
+            title: "Tablespaces",
+            href: "tablespaces",
+            tags: ['schema', 'hypertables', 'manage', 'data', 'timescaledb'],
+            keywords: ['schema', 'hypertables', 'TimescaleDB'],
+            excerpt: ['Create tablespaces on a hypertable']
           }
         ]
       },
@@ -570,19 +677,31 @@ module.exports = [
         children: [
           {
             title: "Migrate from the same database",
-            href: "same-db"
+            href: "same-db",
+            tags: ['migrate', 'data', 'postgresql', 'timescaledb'],
+            keywords: ['migrate', 'data', 'TimescaleDB'],
+            excerpt: ['Migrate data from the same PostgreSQL database']
           },
           {
             title: "Migrate from a different database",
-            href: "different-db"
+            href: "different-db",
+            tags: ['migrate', 'data', 'postgresql', 'timescaledb'],
+            keywords: ['migrate', 'data', 'TimescaleDB'],
+            excerpt: ['Migrate data from a different PostgreSQL database']
           },
           {
             title: "Import from CSV",
-            href: "import-csv"
+            href: "import-csv",
+            tags: ['migrate', 'data', 'timescaledb'],
+            keywords: ['migrate', 'data', 'TimescaleDB'],
+            excerpt: ['Migrate data from a .csv file']
           },
           {
             title: "Migrate InfluxDB data",
-            href: "migrate-influxdb"
+            href: "migrate-influxdb",
+            tags: ['migrate', 'data', 'influxdb', 'timescaledb'],
+            keywords: ['migrate', 'data', 'influxdb', 'TimescaleDB'],
+            excerpt: ['Migrate data from an existing InfluxDB database']
           }
         ]
       },
@@ -592,14 +711,24 @@ module.exports = [
         children: [
           {
             title: "Update from TimescaleDB 1.x to 2.x",
-            href: "update-timescaledb-2"
+            href: "update-timescaledb-2",
+            tags: ['upgrade', '1-x', '2-x', 'timescaledb'],
+            keywords: ['upgrade', '1-x', '2-x','TimescaleDB'],
+            excerpt: ['Upgrade from TimescaleDB 1.x to TimescaleDB 2.x']
           },
           {
-            href: "updating-docker"
+            title: "Update using Docker",
+            href: "updating-docker",
+            tags: ['upgrade', 'docker', '1-x', '2-x', 'timescaledb'],
+            keywords: ['upgrade', 'docker', '1-x', '2-x','TimescaleDB'],
+            excerpt: ['Upgrade from TimescaleDB 1.x to TimescaleDB 2.x using Docker']
           },
           {
             title: "Upgrade PostgreSQL",
-            href: "upgrade-postgresql"
+            href: "upgrade-postgresql",
+            tags: ['upgrade', 'postregsql', 'timescaledb'],
+            keywords: ['upgrade', 'postgresql','TimescaleDB'],
+            excerpt: ['Upgrade the version of PostgreSQL used by TimescaleDB']
           }
         ]
       },
@@ -608,40 +737,69 @@ module.exports = [
         children: [
           {
             title: "Using timescaledb-tune",
-            href: "timescaledb-tune"
+            href: "timescaledb-tune",
+            tags: ['configure', 'timescaledb'],
+            keywords: ['configure', 'timescaledb'],
+            excerpt: ['Configure TimescaleDB using timescaledb-tune']
           },
           {
             title: "Manual PostgreSQL configuration",
-            href: "postgres-config"
+            href: "postgres-config",
+            tags: ['configure', 'postgresql', 'timescaledb'],
+            keywords: ['configure', 'postgresql', 'timescaledb'],
+            excerpt: ['Configure TimescaleDB using the PostgreSQL configuration file']
           },
           {
             title: "TimescaleDB configuration",
-            href: "timescaledb-config"
+            href: "timescaledb-config",
+            tags: ['configure', 'timescaledb'],
+            keywords: ['configure', 'timescaledb'],
+            excerpt: ['Configure TimescaleDB using TimescaleDB configuration parameters']
           },
           {
             title: "Docker configuration",
-            href: "docker-config"
+            href: "docker-config",
+            tags: ['configure', 'docker', 'timescaledb'],
+            keywords: ['configure', 'docker', 'timescaledb'],
+            excerpt: ['Configure TimescaleDB when running within a Docker container']
           },
           {
-            href: "telemetry"
+            title: "Telemetry",
+            href: "telemetry",
+            tags: ['configure', 'telemetry', 'timescaledb'],
+            keywords: ['configure', 'telemetry', 'timescaledb'],
+            excerpt: ['Configure telemetry gathered by TimescaleDB']
           }
         ]
       },
 
       {
-        href: "alerting"
+        title: "Alerting",
+        href: "alerting",
+        tags: ['alert', 'configure', 'timescaledb'],
+        keywords: ['alert', 'configure', 'timescaledb'],
+        excerpt: ['Configure alerting within TimescaleDB']
       },
       {
         title: "Ingest data from other sources",
-        href: "ingest-data"
+        href: "ingest-data",
+        tags: ['ingest', 'data', 'timescaledb'],
+        keywords: ['ingest', 'timescaledb'],
+        excerpt: ['Ingest data into TimescaleDB']
       },
       {
         title: "Troubleshoot TimescaleDB",
-        href: "troubleshoot-timescaledb"
+        href: "troubleshoot-timescaledb",
+        tags: ['tshoot', 'timescaledb'],
+        keywords: ['troubleshoot', 'timescaledb'],
+        excerpt: ['Troubleshooting TimescaleDB']
       },
       {
         title: "Additional tooling",
-        href: "tooling"
+        href: "tooling",
+        tags: ['manage', 'timescaledb'],
+        keywords: ['manage', 'timescaledb'],
+        excerpt: ['Additional tooling for managing TimescaleDB']
       }
     ]
   }
