fb13746685732c43470a6a0df9a2a29c085a9f33
Jacob  Prall <prall.jacob@gmail.com>
2021-08-21 12:41:09 -0500

    change excerpts from arrays to strings

--- timescaledb/getting-started/page-index/page-index.js
+++ timescaledb/getting-started/page-index/page-index.js
@@ -22,27 +22,32 @@ module.exports = [
           {
             title: "4. Add time-series data",
             href: "add-data",
-            exceprt: ""
+            excerpt: "Add sample data to your TimescaleDB instance"
           },
           {
             title: "5. Query your data",
-            href: "query-data"
+            href: "query-data",
+            excerpt: "Query your data using full SQL in TimescaleDB"
           },
           {
             title: "6. Create a continuous aggregate",
-            href: "create-cagg"
+            href: "create-cagg",
+            excerpt: "Establish continuous aggregates in TimescaleDB"
           },
           {
             title: "7. Save space with compression",
-            href: "compress-data"
+            href: "compress-data",
+            excerpt: "Use TimescaleDB's native compression to save space"
           },
           {
             title: "8. Learn about data retention",
-            href: "data-retention"
+            href: "data-retention",
+            excerpt: "Create a database retention policy for your TimescaleDB instance"
           },
           {
             title: "9. Next steps",
-            href: "next-steps"
+            href: "next-steps",
+            excerpt: "Learn how to migrate, visualize and connect your data to TimescaleDB"
           }
         ]
       }
