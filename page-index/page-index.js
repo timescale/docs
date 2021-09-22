64e53a00c764acf164f5773c03f41f1508a63b67
loquacity <github@lanabrindley.com>
2021-09-22 14:57:20 +1000

    Reorganise top-level index

--- page-index/page-index.js
+++ page-index/page-index.js
@@ -1,15 +1,17 @@
 const apiReferencePageIndex = require('../api/page-index/page-index')
+const gettingStartedPageIndex = require('../getting-started/page-index/page-index')
 const timescaleDbPageIndex = require('../timescaledb/page-index/page-index')
 const timescaleForgePageIndex = require('../timescale-forge/page-index/page-index')
 const timescaleCloudPageIndex = require('../timescale-cloud/page-index/page-index')
 const promscalePageIndex = require('../promscale/page-index/page-index')
 
 module.exports = [
+  ...gettingStartedPageIndex,
   ...timescaleDbPageIndex,
-  ...apiReferencePageIndex,
   ...timescaleForgePageIndex,
   ...timescaleCloudPageIndex,
   ...promscalePageIndex,
+  ...apiReferencePageIndex,
   {
     Title: "GitHub",
     type: 'external',
