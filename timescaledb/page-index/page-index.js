82a562dfdbdc8b2b25cccd62378f7170a8caa984
Jacob <prall.jacob@gmail.com>
2021-09-24 12:23:41 -0500

    move install docs up one level, include toolkit under install timescaledb

--- timescaledb/page-index/page-index.js
+++ timescaledb/page-index/page-index.js
@@ -1,9 +1,9 @@
-const overviewPageIndex = require('../overview/page-index/page-index')
-const gettingStartedPageIndex = require('../getting-started/page-index/page-index')
-const howtoPageIndex = require('../how-to-guides/page-index/page-index')
-const quickStartPageIndex = require('../quick-start/page-index/page-index')
-const tutorialPageIndex = require('../tutorials/page-index/page-index')
-
+const overviewPageIndex = require('../overview/page-index/page-index');
+const installPageIndex = require('../install-timescaledb/page-index/page-index');
+const gettingStartedPageIndex = require('../getting-started/page-index/page-index');
+const howtoPageIndex = require('../how-to-guides/page-index/page-index');
+const quickStartPageIndex = require('../quick-start/page-index/page-index');
+const tutorialPageIndex = require('../tutorials/page-index/page-index');
 
 module.exports = [
   {
@@ -13,10 +13,15 @@ module.exports = [
       "All you need to know about TimescaleDB architecture, concepts, setup, how-to's, tutorials, and management",
     children: [
       ...overviewPageIndex,
+      ...installPageIndex,
       ...gettingStartedPageIndex,
       ...howtoPageIndex,
       ...tutorialPageIndex,
       ...quickStartPageIndex,
+      {
+        title: 'TimescaleDB License Comparison',
+        href: 'timescaledb-license-comparison',
+      },
       {
         title: 'Contribute to TimescaleDB',
         href: 'contribute-to-timescaledb',
@@ -24,10 +29,6 @@ module.exports = [
       {
         title: 'Contribute to documentation',
         href: 'contribute-to-docs',
-      },
-      {
-        title: 'TimescaleDB License Comparison',
-        href: 'timescaledb-license-comparison',
       },
     ],
   },
