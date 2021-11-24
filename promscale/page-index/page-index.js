0c11bd00ffe3c81321dac8c922c04d3735a0624b
Jacob Prall <prall.jacob@gmail.com>
2021-11-24 10:10:02 -0600

    move to top level

--- promscale/page-index/page-index.js
+++ promscale/page-index/page-index.js
@@ -1,14 +1,14 @@
 module.exports = [
   {
-    title: 'Promscale',
-    filePath: 'index.md',
-    href: 'promscale',
-    name: 'Promscale',
+    title: "Promscale",
+    filePath: "index.md",
+    href: "promscale",
+    name: "Promscale",
     excerpt:
-      'Promscale is an open source long-term store for Prometheus data designed for analytics, built on top of TimescaleDB.',
+      "Promscale is an open source long-term store for Prometheus data designed for analytics, built on top of TimescaleDB.",
     children: [
       {
-        href: 'install-promscale',
+        href: "install-promscale",
       },
     ],
   },
