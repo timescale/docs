cb34882359247e04b4ff927c3601c64efe1d7ba2
Rajakavitha1 <krajakavitha@gmail.com>
2022-03-01 20:23:51 +0530

    added the imports as jacob suggested

--- timescaledb/page-index/page-index.js
+++ timescaledb/page-index/page-index.js
@@ -1,3 +1,9 @@
+const overviewPageIndex = require('../overview/page-index/page-index')
+const gettingStartedPageIndex = require('../getting-started/page-index/page-index')
+const howtoPageIndex = require('../how-to-guides/page-index/page-index')
+const quickStartPageIndex = require('../quick-start/page-index/page-index')
+const tutorialPageIndex = require('../tutorials/page-index/page-index')
+
 module.exports = [
   {
     title: 'TimescaleDB',
