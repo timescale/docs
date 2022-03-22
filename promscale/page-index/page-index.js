45806e428777f22a201b9b8f141c8e38ff83a0fc
Vineeth Pothulapati <vineethpothulapati@outlook.com>
2022-03-22 08:46:06 +0530

    review iteration 2

--- promscale/page-index/page-index.js
+++ promscale/page-index/page-index.js
@@ -154,7 +154,7 @@ module.exports = [
             href: 'grafana',
             tags: ['promscale', 'configure', 'prometheus', 'metrics'],
             keywords: ['Promscale', 'Grafana'],
-            excerpt: 'Grafana to visualise data in Promscale',
+            excerpt: 'Grafana to visualize data in Promscale',
           },
           {
             title: 'Jaeger',
