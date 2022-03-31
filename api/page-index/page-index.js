92592bdc74448af752ad910ef109ea0d7cab4f98
Charis Lam <26616127+charislam@users.noreply.github.com>
2022-03-31 16:47:21 -0400

    Remove test page

--- api/page-index/page-index.js
+++ api/page-index/page-index.js
@@ -10,7 +10,6 @@ module.exports = [
     description:
       'User-defined actions allow you to run functions and procedures implemented in a language of your choice on a schedule within TimescaleDB. This allows...',
     children: [
-      {href: 'test'},
       {
         title: 'Hypertables & chunks',
         type: 'directory',
