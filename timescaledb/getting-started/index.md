804d0e7b739b5c18afa111b642e404f32f833c64
Miranda Auhl <miranda@timescale.com>
2022-04-12 15:37:19 -0400

    remove install and connecting pages

--- timescaledb/getting-started/index.md
+++ timescaledb/getting-started/index.md
@@ -1,23 +1,24 @@
 # Getting started with TimescaleDB
 
-**Congratulations!** You're ready to jump in and start using TimescaleDB.
+**Congratulations!** You're likely here because you have successfully spun up your first instance of TimescaleDB, can connect to it, and are ready to explore some of the most popular TimescaleDB feautures. If you have not yet created a TimescaleDB instance or learned how to connect to it, make sure to first check out these two sections within the docs:
+ * [Install TimescaleDB][install]
+ * [Connecting to TimescaleDB][connecting]
+
 
 ## What is TimescaleDB?
-Developers often refer to TimescaleDB as PostgreSQL with time-series superpowers.
-Put another way, TimescaleDB is a relational database with native time-series
-optimizations to make ingesting, storing and analyzing time-series data at petabyte
-scale fast and easy. TimescaleDB is packaged as a PostgreSQL extension. It also
-supports all the standard PostgreSQL database objects, like tables, indexes,
-triggers, and more. Moreover, a TimescaleDB database is able to coexist with
-PostgreSQL databases (and other TimescaleDB databases) on a single server.
+TimescaleDB is an extension on top of the PostgreSQL. When you use TimescaleDB, you get all the time-series optimizations and special functionality that TimescaleDB provides, along with all the features available in native PostgreSQL. Developers often refer to TimescaleDB as PostgreSQL with superpowers.
 
-TimescaleDB also supports the full SQL language and you can use TimescaleDB with
-all the tools and connectors in the PostgreSQL ecosystem. If it works with
-PostgreSQL, it works with Timescale!
+TimescaleDB supports the full SQL language and you can use TimescaleDB with
+all the tools and connectors within the PostgreSQL ecosystem. If it works with
+PostgreSQL, it works with TimescaleDB!
 
 ## Let's get up and running
 This Getting Started section gives you a hands-on introduction to the 
 fundamentals of TimescaleDB. Using a real-world dataset, you'll learn definitions
 of key terms (like hypertables and chunks), mental models for working with
 TimescaleDB, and TimescaleDB's key features (like continuous aggregation,
-compression and data retention) and when, why and how to use them.
+data retention, and compression) along when, why, and how to use them.
+
+
+[install]: /install/:currentVersion:/
+[connecting]: /how-to-guides/connecting/
--- timescaledb/getting-started/page-index/page-index.js
+++ timescaledb/getting-started/page-index/page-index.js
@@ -8,74 +8,28 @@ module.exports = [
         excerpt: "Getting started with TimescaleDB",
         children: [
           {
-            title: "1. Launch your first instance",
-            href: "launch-timescaledb",
-            tags: ['learn', 'create', 'tsc'],
-            keywords: ['tutorial', 'Timescale Cloud', 'Docker'],
-            excerpt: "Launch your first TimescaleDB instance",
-            children: [
-            {
-              title: "Launching a Cloud TimescaleDB instance",
-              href: "launch-timescaledb-cloud",
-              tags: ['learn', 'create', 'tsc'],
-              keywords: ['tutorial', 'Timescale Cloud'],
-              excerpt: "Use Timescale Cloud to launch your first TimescaleDB instance",
-            },
-            {
-              title: "Launching TimescaleDB with Docker",
-              href: "launch-timescaledb-docker",
-              tags: ['learn', 'create', 'tsc'],
-              keywords: ['tutorial', 'Docker'],
-              excerpt: "Use Docker to launch your first TimescaleDB instance",
-            },
-            ]
-          },
-          {
-            title: "2. Access your database",
-            href: "access-timescaledb",
-            tags: ['psql', 'connect', 'learn', 'timescaledb'],
-            keywords: ['psql', 'tutorial', 'TimescaleDB'],
-            excerpt: "Connect to your database using psql",
-            children: [
-              {
-                title: "Access TimescaleDB using the terminal",
-                href: "access-timescaledb-terminal",
-                tags: ['psql', 'connect', 'learn', 'timescaledb'],
-                keywords: ['psql', 'tutorial', 'TimescaleDB'],
-                excerpt: "Use the terminal to connect to your TimescaleDB instance",
-              },
-              {
-                title: "Access TimescaleDB using third-party tools",
-                href: "access-timescaledb-third-party-tools",
-                tags: ['psql', 'connect', 'learn', 'timescaledb'],
-                keywords: ['psql', 'tutorial', 'TimescaleDB'],
-                excerpt: "Use third-party tools to connect to your TimescaleDB instance",
-              },
-              ]
-          },
-          {
-            title: "3. Create a hypertable",
+            title: "1. Create a hypertable",
             href: "create-hypertable",
             tags: ['hypertables', 'create', 'learn', 'timescaledb'],
             keywords: ['hypertables', 'tutorial', 'TimescaleDB'],
             excerpt: "Create a hypertable in TimescaleDB"
           },
           {
-            title: "4. Add time-series data",
+            title: "2. Add time-series data",
             href: "add-data",
             tags: ['hypertables', 'data', 'ingest', 'learn', 'timescaledb'],
             keywords: ['hypertables', 'tutorial', 'TimescaleDB'],
             excerpt: "Add sample data to your TimescaleDB instance"
           },
           {
-            title: "5. Query your data",
+            title: "3. Query your data",
             href: "query-data",
             tags: ['data', 'query', 'learn', 'timescaledb'],
             keywords: ['tutorial', 'TimescaleDB'],
             excerpt: "Query your data using full SQL in TimescaleDB"
           },
           {
-            title: "6. Create a continuous aggregate",
+            title: "4. Create a continuous aggregate",
             href: "create-cagg",
             tags: ['caggs', 'create', 'learn', 'timescaledb'],
             keywords: ['continuous aggregates', 'tutorial', 'TimescaleDB'],
@@ -98,21 +52,21 @@ module.exports = [
               ]
           },
           {
-            title: "7. Learn about data retention",
+            title: "5. Learn about data retention",
             href: "data-retention",
             tags: ['data', 'manage', 'learn', 'timescaledb'],
             keywords: ['data', 'tutorial', 'TimescaleDB'],
             excerpt: "Create a database retention policy for your TimescaleDB instance"
           },
           {
-            title: "8. Save space with compression",
+            title: "6. Save space with compression",
             href: "compress-data",
             tags: ['compression', 'data', 'learn', 'timescaledb'],
             keywords: ['compression', 'tutorial', 'TimescaleDB'],
             excerpt: "Use TimescaleDB's native compression to save space"
           },
           {
-            title: "9. Next steps",
+            title: "7. Next steps",
             href: "next-steps",
             tags: ['migrate', 'visualize', 'manage', 'learn', 'timescaledb'],
             keywords: ['tutorial', 'TimescaleDB'],
