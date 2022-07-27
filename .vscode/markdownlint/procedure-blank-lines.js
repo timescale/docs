c43fcd0625a55a21a99d504a1fa36e02fdfaf27d
Charis Lam <26616127+charislam@users.noreply.github.com>
2022-07-27 14:29:22 -0400

    Check for blank line after imports
    Add markdownlint rule to check for blank lines after imports, and insert one on auto-fix if missing. Refactor to move common functions into a utilities file.

--- .vscode/markdownlint/procedure-blank-lines.js
+++ .vscode/markdownlint/procedure-blank-lines.js
@@ -1,54 +1,10 @@
 'use strict';
 
-/*
- * Checks if a line is blank (contains no characters or only whitespace).
- *
- * @param {string} line The line to check.
- * @returns {boolean}
- */
-const isBlank = (line) => line.match(new RegExp('^\s*$'));
-
-/*
- * Gets all the tokens that correspond to procedure tags.
- * 
- * @param {Object[]} tokens The tokens to check. May have nested token arrays.
- * @returns {Object[]} Tokens corresponding to procedure tags. Non-nested.
- */
-const findProcedureTags = (tokens) => {
-  const procedureTags = [];
-  const recursiveFindProcedureTags = (tokens) => {
-    tokens.map((token) => {
-      if (token.children) {
-        recursiveFindProcedureTags(token.children);
-      } else if (
-        token.content &&
-          token.content.match(new RegExp('<\/?procedure>'))
-      ) {
-        procedureTags.push(token);
-      }
-    });
-  };
-  recursiveFindProcedureTags(tokens);
-  return procedureTags;
-};
-
-/*
- * Adds a markdownlint error with a corresponding fix that inserts blank lines.
- * 
- * @param {addErrorCallback} onError The callback that adds markdownlint errors.
- * @param {number} procedureLine Zero-indexed line number of procedure tag.
- * @param {number} blankLineOffset Relative line on which to add blank line.
- */
-const addErrorAndInsertBlank = (onError, procedureLine, blankLineOffset) => {
-  onError({
-    lineNumber: procedureLine + 1, // lineNumber is indexed from 1
-    fixInfo: {
-      'insertText': '\n',
-      'lineNumber': procedureLine + 1 + blankLineOffset,
-      'editColumn': 1
-    }
-  })
-};
+const {
+  addErrorAndInsertBlank,
+  findPatternInContent,
+  isBlank 
+} = require('./utils');
 
 /*
  * Check for blank lines on either side of a procedure tag. Registers an error
@@ -59,7 +15,8 @@ const addErrorAndInsertBlank = (onError, procedureLine, blankLineOffset) => {
  */
 const checkProcedureBlankLines = (params, onError) => {
   const { lines } = params;
-  const procedureTags = findProcedureTags(params.tokens);
+  const procedureTags = findPatternInContent(params.tokens, '<\/?procedure>');
+  if (procedureTags.length === 0) return;
   procedureTags.forEach((procedureTag) => {
     // lineNumber is indexed from 1
     const procedureLine = procedureTag.lineNumber - 1;
@@ -68,7 +25,7 @@ const checkProcedureBlankLines = (params, onError) => {
     const hasFollowingBlank = isBlank(lines[procedureLine + 1]);
     if (!hasPrecedingBlank) addErrorAndInsertBlank(onError, procedureLine, 0);
     if (!hasFollowingBlank) addErrorAndInsertBlank(onError, procedureLine, 1);
-  })
+  });
 };
 
 module.exports = {
