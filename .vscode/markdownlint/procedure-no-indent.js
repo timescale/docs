65c526c9bc768b3370e2416e280c4199a0cb4c3c
Charis Lam <26616127+charislam@users.noreply.github.com>
2022-07-28 11:29:41 -0400

    Add rule and fix for indented procedure tags

---
+++ .vscode/markdownlint/procedure-no-indent.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const {
+  findPatternInContent,
+} = require('./utils');
+
+/*
+ * Check for indent of a procedure tag. Registers an error and fix with
+ * markdownlint if tag is indented.
+ * 
+ * @param {Object} params Parsed Markdown content, provided by markdownlint.
+ * @param {addErrorCallback} onError The callback that adds markdownlint errors.
+ */
+const checkProcedureNoIndent = (params, onError) => {
+  const procedureTags = findPatternInContent(params.tokens, '<\/?procedure>');
+  if (procedureTags.length === 0) return;
+  for (let i = 0; i < procedureTags.length; i++) {
+    const procedureTagText = procedureTags[i].line;
+    const numberWhitespaceCharacters =
+      procedureTagText.length - procedureTagText.trimStart().length;
+    if (numberWhitespaceCharacters === 0) continue;
+    onError({
+      lineNumber: procedureTags[i].lineNumber,
+      fixInfo: {
+        'deleteCount': numberWhitespaceCharacters,
+      },
+    });
+  }
+};
+
+module.exports = {
+  names: [ 'TS004', 'timescale.procedures-no-indent' ],
+  description: 'Procedure tags should not be indented',
+  tags: [ 'validation', 'procedures' ],
+  function: checkProcedureNoIndent
+};
