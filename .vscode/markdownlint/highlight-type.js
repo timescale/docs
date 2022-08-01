2d7034345b9e95e57d87db0e555e176eea4eaeb8
Charis Lam <26616127+charislam@users.noreply.github.com>
2022-08-01 14:17:13 -0400

    Check for correct highlight type

---
+++ .vscode/markdownlint/highlight-type.js
@@ -0,0 +1,54 @@
+// @ts-check
+
+const { findPatternInLines } = require('./utils');
+
+const highlightTypes = [
+  'note',
+  'important',
+  'warning',
+  'cloud',
+  'deprecation',
+];
+
+const errorMessage = highlightTypes.reduce((message, type, index) => (
+  message.concat(`\`${type}\` ${index === highlightTypes.length - 1 ? '.' : ', '}`)
+), 'Allowed highlight types are ');
+
+/*
+ * Check for blank lines following an import statement. Registers an error
+ * and fix with markdownlint if blank lines are missing.
+ * 
+ * @param {Object} params Parsed Markdown content, provided by markdownlint.
+ * @param {addErrorCallback} onError The callback that adds markdownlint errors.
+ */
+const checkHighlightType = (params, onError) => {
+  const { lines } = params;
+  const highlightTags = findPatternInLines(lines, '<highlight[^<]*>');
+
+  highlightTags.forEach((tag) => {
+    const match = tag.line.match('type="([^"]+)"');
+
+    if (!match) {
+      onError({
+        lineNumber: tag.lineNumber,
+        detail: 'No type defined for highlight',
+      });
+      return;
+    }
+
+    const highlightType = match.at(1);
+    if (!highlightTypes.includes(highlightType)) {
+      onError({
+        lineNumber: tag.lineNumber,
+        detail: `The highlight type \`${highlightType}\` is not allowed.`
+      });
+    }
+  });
+};
+
+module.exports = {
+  names: [ 'TS008', 'timescale.highlight-types' ],
+  description: errorMessage,
+  tags: [ 'validation', 'highlights' ],
+  function: checkHighlightType
+};
