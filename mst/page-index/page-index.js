c3b004af77ad6944d77541afc6f016c55e41401c
loquacity <github@lanabrindley.com>
2022-08-29 13:36:14 +1000

    Trying with just a single page instead

--- mst/page-index/page-index.js
+++ mst/page-index/page-index.js
@@ -12,18 +12,8 @@ module.exports = [
         href: "about-mst",
       },
       {
-        title: "Command Line tool for Managed Service for TimescaleDB",
-        href: "aiven-client",
-        children: [
-          {
-            title: "Aiven Client for Managed Service for TimescaleDB",
-            href: "aiven-client-install",
-          },
-          {
-            title: "Create a fork using Aiven Client",
-            href: "create-fork",
-          },
-       ],
+        title: "Aiven Client for Managed Service for TimescaleDB",
+        href: "aiven-client-install",
       },
       {
         title: "Clouds and regions",
