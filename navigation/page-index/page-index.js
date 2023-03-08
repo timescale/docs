7fe384b34b5b00041348ce4741bc2863c3e4040e
Charis Lam <26616127+charislam@users.noreply.github.com>
2023-03-07 17:36:36 -0800

    define a nav section in the main index

---
+++ navigation/page-index/page-index.js
@@ -0,0 +1,27 @@
+module.exports = [
+  {
+    title: "Find a docs page",
+    href: "navigation",
+    filePath: "index.md",
+    children: [
+      {
+        title: "Find pages by Cloud Console location",
+        overrideHref: "/cloud-console",
+        excerpt: "Browse topics by Timescale Cloud console location.",
+        type: "placeholder",
+      },
+      {
+        title: "Find by keyword",
+        overrideHref: "/keywords",
+        excerpt: "Browse topics by keywords.",
+        type: "placeholder",
+      },
+      {
+        title: "Full search",
+        overrideHref: "/search",
+        excerpt: "Search Timescale docs, blog, and forum.",
+        type: "placeholder",
+      },
+    ],
+  },
+];
