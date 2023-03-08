711ac11ea408ea19456c954378b131ebfc14a7ba
Charis <26616127+charislam@users.noreply.github.com>
2023-03-07 19:20:58 -0800

    define a nav section in the main index (#2105)
    * define a nav section in the main index
    
    * Apply suggestions from code review
    
    Co-authored-by: Lana Brindley <github@lanabrindley.com>
    Signed-off-by: Charis <26616127+charislam@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Charis <26616127+charislam@users.noreply.github.com>
    Co-authored-by: Lana Brindley <github@lanabrindley.com>

--- page-index/page-index.js
+++ page-index/page-index.js
@@ -3,7 +3,7 @@ const selfHostedPageIndex = require("../self-hosted/page-index/page-index");
 const timescaleMSTPageIndex = require("../mst/page-index/page-index");
 const gsgPageIndex = require("../getting-started/page-index/page-index");
 const timescaleUsingPageIndex = require("../use-timescale/page-index/page-index");
-const timescaleCloudPageIndex = require("../cloud-console/page-index/page-index");
+const navigationPageIndex = require("../navigation/page-index/page-index");
 const tutorialsPageIndex = require("../tutorials/page-index/page-index.js");
 const codeQuickStartsPageIndex = require("../quick-start/page-index/page-index.js");
 const timescaleAboutPageIndex = require("../about/page-index/page-index");
@@ -12,7 +12,6 @@ const promscalePageIndex = require("../promscale/page-index/page-index");
 module.exports = [
   ...gsgPageIndex,
   ...timescaleUsingPageIndex,
-  ...timescaleCloudPageIndex,
   ...tutorialsPageIndex,
   ...codeQuickStartsPageIndex,
   ...apiReferencePageIndex,
@@ -20,6 +19,7 @@ module.exports = [
   ...selfHostedPageIndex,
   ...promscalePageIndex,
   ...timescaleAboutPageIndex,
+  ...navigationPageIndex,
   {
     Title: "GitHub",
     type: "external",
