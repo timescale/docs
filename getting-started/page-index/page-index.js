5eb8bf5f3587e99efa2ec214dbd0952123d21d2c
loquacity <github@lanabrindley.com>
2023-05-15 15:27:31 +0200

    Initial file setup

--- getting-started/page-index/page-index.js
+++ getting-started/page-index/page-index.js
@@ -6,54 +6,34 @@ module.exports = [
     excerpt: "Get started with Timescale",
     children: [
       {
-        title: "Create a hypertable",
-        href: "create-hypertable",
-        excerpt: "Create a hypertable in Timescale",
+        title: "Timescale Services",
+        href: "services",
+        excerpt: "Create a Timescale service and connect to it",
       },
       {
-        title: "Add time-series data",
-        href: "add-data",
-        excerpt: "Add sample data to your Timescale instance",
+        title: "Tables and hypertables",
+        href: "tables-hypertables",
+        excerpt: "Create tables and hypertables for your data",
       },
       {
-        title: "Query your data",
+        title: "Time-series data",
+        href: "time-series-data",
+        excerpt: "Add time-series data to your Timescale database",
+      },
+      {
+        title: "Queries",
         href: "query-data",
         excerpt: "Query your data using full SQL in Timescale",
       },
       {
-        title: "Create a continuous aggregate",
-        href: "create-cagg",
-        excerpt: "Establish continuous aggregates in Timescale",
-        children: [
-          {
-            title: "Continuous aggregate basics",
-            href: "create-cagg-basics",
-            excerpt: "Create continuous aggregates in Timescale",
-          },
-          {
-            title: "Continuous aggregate policies and refreshing data",
-            href: "create-cagg-policy",
-            excerpt: "Establish continuous aggregates policy in Timescale",
-          },
-        ],
+        title: "Aggregation",
+        href: "aggregation",
+        excerpt: "Query aggregated data, and create a continuous aggregate",
       },
-      {
-        title: "Save space with compression",
-        href: "compress-data",
-        excerpt: "Use Timescale's native compression to save space",
-      },
-      {
-        title: "Learn about data retention",
-        href: "data-retention",
-        excerpt:
-          "Create a database retention policy for your Timescale instance",
-      },
-
       {
         title: "Next steps",
         href: "next-steps",
-        excerpt:
-          "Learn how to migrate, visualize and connect your data to Timescale",
+        excerpt: "Find out how to get more from your Timescale database",
       },
     ],
   },
---
+++ getting-started/services.md
@@ -0,0 +1,48 @@
+---
+title: Get started with Timescale
+excerpt: Get started with your first Timescale instance
+products: [cloud]
+---
+
+import Install from "versionContent/_partials/_cloud-installation.mdx";
+import CreateService from "versionContent/_partials/_cloud-create-service.mdx";
+import Connect from "versionContent/_partials/_cloud-connect.mdx";
+import CloudIntro from "versionContent/_partials/_cloud-intro.mdx";
+import TimescaleIntro from "versionContent/_partials/_timescale-intro.mdx";
+import WhereNext from "versionContent/_partials/_where-to-next.mdx";
+import CloudTrial from "versionContent/_partials/_cloudtrial.mdx";
+
+# Get started with Timescale
+
+<TimescaleIntro />
+
+<CloudIntro />
+
+This guide helps you set up a Timescale database, so you can work with some
+real-time stock trading data, provided by [Twelve Data][twelve-data].
+
+If you have any questions or concerns as you go through the tutorial, check out
+the Timescale community [Slack][slack] and [Timescale Forum][forum], where you
+can find help from the Timescale community and team.
+
+<CloudTrial />
+
+## Create your Timescale account
+
+<Install />
+
+## Create your first service
+
+<CreateService demoData={false} />
+
+## Connect to your service
+
+<Connect />
+
+## Where to next
+
+<WhereNext />
+
+[forum]: https://www.timescale.com/forum
+[slack]: https://slack.timescale.com/
+[twelve-data]: https://twelvedata.com/
