148592383602e1298c012a26c6eca422c2d656c0
loquacity <github@lanabrindley.com>
2023-08-28 16:07:53 +1000

    Bring back the pg service page

--- use-timescale/page-index/page-index.js
+++ use-timescale/page-index/page-index.js
@@ -28,6 +28,11 @@ module.exports = [
             excerpt: "Timescale services",
           },
           {
+            title: "Create a PostgreSQL service",
+            href: "create-pg-service",
+            excerpt: "PostgreSQL services",
+          },
+          {
             title: "Service explorer",
             href: "service-explorer",
             excerpt: "Timescale services explorer",
