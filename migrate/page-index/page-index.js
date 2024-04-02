fd165c2b4d127d625c75ef505062ab42f006969f
Konstantinos Gkiokas <giokostis@gmail.com>
2024-04-02 13:10:17 +0300

    Move multinode migration under playbooks (#3110)

--- migrate/page-index/page-index.js
+++ migrate/page-index/page-index.js
@@ -11,12 +11,6 @@ module.exports = [
             "Migrate a hypertable or entire database with native PostgreSQL commands",
       },
       {
-        title: "With downtime: Multi-node to TimescaleDB service",
-        href: "multi-node-to-timescale-service",
-        excerpt:
-            "Migrate an entire multi-node deployment to a TimescaleDB service",
-      },
-      {
         title: "Low-downtime: Live migration",
         href: "live-migration",
         excerpt: "Migrate a large database with low downtime",
@@ -82,6 +76,12 @@ module.exports = [
             href: "rds-timescale-live-migration",
             excerpt:
                 "Migrate from RDS to Timescale using live migration",
+          },
+          {
+            title: "Multi-node to Timescale",
+            href: "multi-node-to-timescale-service",
+            excerpt:
+                "Migrate an entire multi-node deployment to Timescale",
           },
         ],
       },
