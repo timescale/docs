4996e446907dbc5baf42b4d6a007b3e2bf6f3442
Anagha Mittal <43825882+anaghamittal@users.noreply.github.com>
2025-02-27 20:00:51 +0530

    Integrations: Power BI (#3857)
    Signed-off-by: Anagha Mittal <43825882+anaghamittal@users.noreply.github.com>
    Co-authored-by: atovpeko <anastasiatverdokhleb@gmail.com>
    Co-authored-by: Iain Cox <iain@timescale.com>

--- use-timescale/page-index/page-index.js
+++ use-timescale/page-index/page-index.js
@@ -811,6 +811,11 @@ module.exports = [
             excerpt: "Integrate Prometheus with Timescale Cloud",
           },
           {
+            title: "Power BI",
+            href: "power-bi",
+            excerpt: "Integrate Power BI with Timescale Cloud",
+          },
+          {
             title: "psql",
             href: "psql",
             excerpt: "Connect to Timescale products with psql",
