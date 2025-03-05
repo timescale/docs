069b2f7fffd7e45d3a16c558bacb19e6b40a18a1
Anastasiia Tovpeko <114177030+atovpeko@users.noreply.github.com>
2025-03-05 17:04:58 +0200

    Connect from any cloud with AWS TGW (#3893)
    * draft
    
    * trigger build
    
    * update with partial
    
    * Update use-timescale/security/transit-gateway.md
    
    Co-authored-by: Iain Cox <iain@timescale.com>
    Signed-off-by: Anastasiia Tovpeko <114177030+atovpeko@users.noreply.github.com>
    
    * Update use-timescale/security/transit-gateway.md
    
    Co-authored-by: Iain Cox <iain@timescale.com>
    Signed-off-by: Anastasiia Tovpeko <114177030+atovpeko@users.noreply.github.com>
    
    * Update use-timescale/security/transit-gateway.md
    
    Co-authored-by: Iain Cox <iain@timescale.com>
    Signed-off-by: Anastasiia Tovpeko <114177030+atovpeko@users.noreply.github.com>
    
    * review
    
    * review
    
    * review
    
    * chore: reuse and numbering update.
    
    * review
    
    ---------
    
    Signed-off-by: Anastasiia Tovpeko <114177030+atovpeko@users.noreply.github.com>
    Co-authored-by: Iain Cox <iain@timescale.com>

--- use-timescale/page-index/page-index.js
+++ use-timescale/page-index/page-index.js
@@ -789,6 +789,11 @@ module.exports = [
             excerpt: "Integrate Amazon SageMaker with Timescale Cloud",
           },
           {
+            title: "Amazon Web Services",
+            href: "aws",
+            excerpt: "Integrate AWS with Timescale Cloud",
+          },
+          {
             title: "Apache Airflow",
             href: "apache-airflow",
             excerpt: "Integrate Apache Airflow with Timescale products",
@@ -807,6 +812,11 @@ module.exports = [
             title: "Azure Data Studio",
             href: "azure-data-studio",
             excerpt: "Integrate Azure Data Studio with Timescale Cloud",
+          },
+          {
+            title: "Corporate data center",
+            href: "corporate-data-center",
+            excerpt: "Integrate your corporate data center with Timescale Cloud",
           },
           {
             title: "Datadog",
@@ -829,9 +839,19 @@ module.exports = [
             excerpt: "Integrate Fivetran with Timescale Cloud",
           },
           {
+            title: "Google Cloud",
+            href: "google-cloud",
+            excerpt: "Integrate Google Cloud with Timescale Cloud",
+          },
+          {
             title: "Grafana",
             href: "grafana",
             excerpt: "Integrate Grafana with Timescale products",
+          },
+          {
+            title: "Microsoft Azure",
+            href: "microsoft-azure",
+            excerpt: "Integrate Microsoft Azure with Timescale Cloud",
           },
           {
             title: "pgAdmin",
