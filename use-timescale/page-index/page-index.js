ef8f0c6697dae05e915f85c4d6e222c1c40790f1
Iain Cox <iain@timescale.com>
2025-03-18 19:03:44 +0100

    Secondary index docs (#3943)
    * chore: ready for first review.

--- use-timescale/page-index/page-index.js
+++ use-timescale/page-index/page-index.js
@@ -208,6 +208,11 @@ module.exports = [
             href: "modify-data-in-hypercore",
             excerpt: "Update data stored in the columnstore",
           },
+          {
+            title: "Improve query and upsert performance",
+            href: "secondary-indexes",
+            excerpt: "Automate",
+          },
         ],
       },
       {
