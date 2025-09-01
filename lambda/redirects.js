320881564552ec42ac9e1a4f6e3a9796a023ecce
billy-the-fish <iain@tigerdata.com>
2025-09-01 12:41:23 +0200

    chore: replace troubleshooting in compression with the hypercore one.

--- lambda/redirects.js
+++ lambda/redirects.js
@@ -930,6 +930,10 @@ module.exports = [
     to: "https://docs.timescale.com/use-timescale/latest/hypercore/compression-methods/"
   },
   {
+    from: "/use-timescale/latest/compression/troubleshooting/",
+    to: "https://docs.tigerdata.com/use-timescale/latest/hypercore/troubleshooting/"
+  },
+  {
     from: "/use-timescale/latest/integrations/observability-alerting/grafana/installation/",
     to: "https://docs.tigerdata.com/integrations/latest/grafana/"
   },
