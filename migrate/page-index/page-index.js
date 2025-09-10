c8770a312ac3e50049801b7e6082449875301b41
atovpeko <anastasiatverdokhleb@gmail.com>
2025-09-10 21:58:24 +0300

    update

--- migrate/page-index/page-index.js
+++ migrate/page-index/page-index.js
@@ -25,53 +25,41 @@ module.exports = [
         excerpt: "Upload files into a Tiger Cloud service using the terminal",
       },
       {
-        title: "Migrate data",
-        href: "migrate-data",
-        excerpt: "Migrate your data into Tiger Cloud",
+        title: "Migrate with downtime",
+        href: "pg-dump-and-restore",
+        excerpt: "Migrate a hypertable or entire database with native Postgres commands",
+      },
+      {
+        title: "Live migration",
+        href: "live-migration",
+        excerpt: "Migrate a large database with low downtime",
+      },
+      {
+        title: "Dual-write and backfill",
+        href: "dual-write-and-backfill",
+        excerpt: "Migrate a large database with low downtime",
         children: [
-          {
-            title: "Migrate with downtime",
-            href: "pg-dump-and-restore",
-            excerpt: "Migrate a hypertable or entire database with native Postgres commands",
-          },
-          {
-            title: "Live migration",
-            href: "live-migration",
-            excerpt: "Migrate a large database with low downtime",
-          },
-          {
-            title: "Dual-write and backfill",
-            href: "dual-write-and-backfill",
-            excerpt: "Migrate a large database with low downtime",
-            children: [
               {
                 title: "Dual-write from TimescaleDB",
                 href: "dual-write-from-timescaledb",
-                excerpt:
-                  "Migrate from TimescaleDB using dual-write and backfill",
+                excerpt: "Migrate from TimescaleDB using dual-write and backfill",
               },
               {
                 title: "Dual-write from Postgres",
                 href: "dual-write-from-postgres",
-                excerpt:
-                  "Migrate from Postgres using dual-write and backfill",
+                excerpt: "Migrate from Postgres using dual-write and backfill",
               },
               {
                 title: "Dual-write from other databases",
                 href: "dual-write-from-other",
-                excerpt:
-                  "Migrate from other databases using dual-write and backfill",
+                excerpt: "Migrate from other databases using dual-write and backfill",
               },
               {
                 title: "timescaledb-backfill",
                 href: "timescaledb-backfill",
-                excerpt:
-                  "A tool for backfilling data as part of data migration",
+                excerpt: "A tool for backfilling data as part of data migration",
               },
-
-            ],
-          },
-        ],
+          ],
       },
       {
         title: "FAQ and troubleshooting",
