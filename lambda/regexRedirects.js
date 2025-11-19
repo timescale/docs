1019c11421b803dbb6d36ff1b76cc675fee28207
robsoncoelho <robson.amc@gmail.com>
2025-11-18 22:09:20 -0300

    chore: update redirects to point to docs subdirectory

--- lambda/regexRedirects.js
+++ lambda/regexRedirects.js
@@ -1,23 +1,23 @@
 module.exports = [
   {
     from: /^\/v[0-1]\.[0-9]\/api/,
-    to: "https://docs.tigerdata.com/api/latest/"
+    to: "https://www.tigerdata.com/docs/api/latest/"
   },
   {
     from: /^\/v[0-1]\.[0-9]\/getting-started/,
-    to: "https://docs.tigerdata.com/getting-started/latest/"
+    to: "https://www.tigerdata.com/docs/getting-started/latest/"
   },
   {
     from: /^\/use-timescale\/latest\/integrations(\/.*)?$/,
     to: (match) =>
-      `https://docs.tigerdata.com/integrations/latest${match[1] || ""}`
+      `https://www.tigerdata.com/docs/integrations/latest${match[1] || ""}`
   },
   {
     from: /^\/quick-start\/latest\/(\/.*)?$/,
-    to: `https://docs.tigerdata.com/getting-started/latest/start-coding-with-timescale/`
+    to: `https://www.tigerdata.com/docs/getting-started/latest/start-coding-with-timescale/`
   },
   {
     from: /^\/v[0-1]\.[0-9]\/tutorials/,
-    to: "https://docs.tigerdata.com/tutorials/latest/"
+    to: "https://www.tigerdata.com/docs/tutorials/latest/"
   }
 ];
